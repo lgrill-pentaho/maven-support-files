a5e3f785554ef06ebab04f4eae9fd7de856839ae
Larry Grill <lgrill@pentaho.com>
2016-07-22 16:00:15 -0400

    multiline shell

--- shellstuff.sh
+++ shellstuff.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -ev
 ls -al
+git clone https://github.com/lgrill-pentaho/maven-support-files.git
+mvn install --settings maven-support-files/settings.xml -DskipTests=true -Dmaven.javadoc.skip=true -Dorg.apache.maven.user-settings=maven-support-files/settings.xml -B -V
+
